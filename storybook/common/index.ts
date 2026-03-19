1e2dce52ab9b574ef3cd4f6a11967302aaa768e8
Hatem Hosny <hatemhosny@gmail.com>
2026-03-19 17:58:48 +0200

    update docs to add new SDKs

--- storybook/common/index.ts
+++ storybook/common/index.ts
@@ -2,7 +2,7 @@ import './deps';
 
 export * from './arg-types';
 export * from './internal';
-export * from './livecodes';
+export type * from './livecodes';
 export type * from './story-definition';
 export * from './themes';
 export * from './utils';
